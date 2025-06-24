4cf7295822e9520d2c995d2bd3fd2322d493b1a1
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-06-24 15:29:27 +0530

    edit in extension

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,17 @@ function App() {
   return (
     // <div className="">
     <div
-      className={`transition-all  duration-300 h-screen   ${isDark ? "bg-gray-900 text-white" : "bg-blue-50 text-black"
-        }`}
+      className={`transition-all  pt-10 pb-10  md:px-40 duration-300 lg:h-screen   ${
+        isDark ? "bg-gray-900 text-white" : "bg-blue-50 text-black"
+      }`}
     >
       {/* Header */}
       <header
-        className={`flex px-4  py-3 justify-between md:items-center border-2 md:px-6 md:py-3 rounded-lg shadow-md mb-8 transition-colors duration-300 ${isDark ? "bg-gray-900" : "bg-white"
-          }`}
+        className={`flex justify-between md:items-center  px-5 py-3 border-2 rounded-lg shadow-md mb-8 transition-colors duration-300 ${
+          isDark ? "bg-gray-900" : "bg-white"
+        }`}
       >
-        <img className="w-25 md:w-50" src={Icon.logo} alt="Logo" />
+        <img className="w-25 md:w-40" src={Icon.logo} alt="Logo" />
         <button onClick={changetheme} className="focus:outline-none">
           <img
             className="w-6 cursor-pointer"
@@ -33,33 +35,35 @@ function App() {
       </header>
       <div>
         <h2
-          className={`text-2xl font-bold mb-10 ${isDark ? "color-black" : "color-white"
-            }`}
+          className={`text-2xl font-bold mb-10 ${
+            isDark ? "color-black" : "color-white"
+          }`}
         >
           Extension
         </h2>
       </div>
       {/* Grid Section */}
-      <div className="sm:px-20 md:px-10 lg:px-20 xl:px-40 2xl:px-100">
+      <div className="sm:px-20 md:px-10 lg:px-20 xl:px-40 2xl:px-40">
         <div className="grid px-20 sm:px-0   sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {extensions.map((ext, index) => (
             <div
               key={index}
-              className={` ${isDark ? "text-white bg-gray-800" : "text-gray bg-white"
-                } flex flex-row items-start border  rounded-md p-4   shadow-sm`}
+              className={` ${
+                isDark ? "text-white bg-gray-800" : "text-gray bg-white"
+              } flex flex-row items-start border  rounded-md p-4   shadow-sm`}
             >
               {/* <div classname=""> */}
-                <div classname="flex">
-                  <img
-                    src={ext.logo}
-                    alt={ext.name}
-                    className="w-10 h-10 rounded-md mb-2"
-                  />
+              <div classname="flex">
+                <img
+                  src={ext.logo}
+                  alt={ext.name}
+                  className="w-10 h-10 rounded-md mb-2"
+                />
                 {/* </div> */}
                 {/* <div> */}
-                  <h2 className="text-md font-semibold">{ext.name}</h2>
-                  <p className="text-xs font-normal">{ext.description}</p>
-                </div>
+                <h2 className="text-md font-semibold">{ext.name}</h2>
+                <p className="text-xs font-normal">{ext.description}</p>
+              </div>
               {/* </div> */}
               <span
                 className={`mt-2 text-xs font-medium ${
